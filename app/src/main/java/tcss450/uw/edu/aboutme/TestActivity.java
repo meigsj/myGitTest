7eee12145ff16aa5a5ba328f00958a59de891ccc
Joshua <joshua.meigs@gmail.com>
2017-01-17 12:28:45 -0800

    Added a good Comment...

--- app/src/main/java/tcss450/uw/edu/aboutme/TestActivity.java
+++ app/src/main/java/tcss450/uw/edu/aboutme/TestActivity.java
@@ -9,5 +9,6 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        //This is my comment...
     }
 }
